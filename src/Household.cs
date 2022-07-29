473fdbe740c11a7308cd6a6bdcb0151e8eb3d7f1
john pierson <sixtysecondrevit@gmail.com>
2022-07-29 11:42:10 -0600

    Upload of open source code

---
+++ src/Household.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomClasses
+{
+    public class HouseholdItem
+    {
+        public double Size { get; set; }
+        public double Price { get; set; }
+
+        public HouseholdItem(double size, double price)
+        {
+            Size = size;
+            Price = price;
+        }
+
+        public HouseholdItem()
+        {
+            Size = 1;
+        }
+    }
+
+    public class PeanutWithShell : HouseholdItem
+    {
+        public PeanutWithShell()
+        {
+            Size = 10;
+        }
+    }
+    public class PintoBean : HouseholdItem
+    {
+        public PintoBean()
+        {
+            Size = 5;
+        }
+    }
+    public class TableSalt : HouseholdItem
+    {
+        public TableSalt()
+        {
+            Size = 1;
+        }
+    }
+}
